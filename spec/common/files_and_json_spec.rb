03976157c05fe81912244216a4c54a7a006729c1
Zsolt Fabok <me@zsoltfabok.com>
2014-07-10 22:38:12 +0200

    remove whitespaces

--- spec/common/files_and_json_spec.rb
+++ spec/common/files_and_json_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe Common::FilesAndJson do
-	it "writes the argument data to a file in json" do
-		json_output = "output"
-		hash = {}
-		file = double
-		expect(JSON).to receive(:pretty_generate).with(hash).and_return(json_output)
-		expect(File).to receive(:open).with("filename", "w").and_yield(file)
-		expect(file).to receive(:write).with(json_output)
-		Common::FilesAndJson.new.to_file(hash, "filename")
-	end
+  it "writes the argument data to a file in json" do
+    json_output = "output"
+    hash = {}
+    file = double
+    expect(JSON).to receive(:pretty_generate).with(hash).and_return(json_output)
+    expect(File).to receive(:open).with("filename", "w").and_yield(file)
+    expect(file).to receive(:write).with(json_output)
+    Common::FilesAndJson.new.to_file(hash, "filename")
+  end
 end
